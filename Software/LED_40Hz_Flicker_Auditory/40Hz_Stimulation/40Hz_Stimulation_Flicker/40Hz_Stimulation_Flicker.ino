63a4ef564e2d25e69693e7cbb9c2aa32e5318578
Yonathan Aberra <yonathan.aberra@gmail.com>
2019-06-10 16:48:16 -0700

    update

--- Software/LED_40Hz_Flicker_Auditory/40Hz_Stimulation/40Hz_Stimulation_Flicker/40Hz_Stimulation_Flicker.ino
+++ Software/LED_40Hz_Flicker_Auditory/40Hz_Stimulation/40Hz_Stimulation_Flicker/40Hz_Stimulation_Flicker.ino
@@ -1,15 +1,12 @@
 void setup() {
   pinMode(2, OUTPUT);   // Chose most convenient pin for your set up
 }
-int frequency = 1000/40;
+
 void loop() {
-  digitalWrite(2, HIGH);   // turn the LED strip on 
-  delay(frequency);                       // wait for a second
-  digitalWrite(2, LOW);    // turn the LED strip off
-  delay(frequency);                       // wait for a second
-  // turn off tone function for pin 7:
-  noTone(7);
-  // play a note on pin 6 for 200 ms:
-  tone(7, 10000, 1);
-  delay(frequency);
+  int stim = 1000/40; // stimulation frequency 
+   digitalWrite(2, HIGH);   // turns the LED strip on at stimulation frequency
+  delay(stim);
+  digitalWrite(2, LOW);
+  delay(stim);
+  
 }
