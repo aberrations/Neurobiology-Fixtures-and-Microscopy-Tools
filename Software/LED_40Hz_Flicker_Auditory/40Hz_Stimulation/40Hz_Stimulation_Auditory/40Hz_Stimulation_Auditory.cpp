c920731d2d34a145f515ba6b5da5682a2de85103
Yonathan Aberra <yonathan.aberra@gmail.com>
2019-06-13 11:17:57 -0700

    update

--- Software/LED_40Hz_Flicker_Auditory/40Hz_Stimulation/40Hz_Stimulation_Auditory/40Hz_Stimulation_Auditory.cpp
+++ Software/LED_40Hz_Flicker_Auditory/40Hz_Stimulation/40Hz_Stimulation_Auditory/40Hz_Stimulation_Auditory.cpp
@@ -3,6 +3,6 @@ void setup() {
 
 void loop() {
 noTone(7);    // turn the tone on at stimulation frequency
-  tone(7, 10000, 1); // 10kHz tone on pin 7 for 1 ms:
-  delay(stim); 
+tone(7, 10000, 1); // 10kHz tone on pin 7 for 1 ms:
+delay(stim); 
 }
--- Software/LED_40Hz_Flicker_Auditory/40Hz_Stimulation/40Hz_Stimulation_Combined/40Hz_Stimulation_Combined.cpp
+++ Software/LED_40Hz_Flicker_Auditory/40Hz_Stimulation/40Hz_Stimulation_Combined/40Hz_Stimulation_Combined.cpp
@@ -3,7 +3,8 @@ void setup() {
 }
 
 void loop() {
-  int stim = 1000/40; // stimulation frequency 
+  int stim = 1000/40; // stimulation frequency
+
   digitalWrite(2, HIGH);   // turns the LED strip on at stimulation frequency
   delay(stim);
   digitalWrite(2, LOW);
